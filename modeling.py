2ae8aed59c003f56aada14f084d41d0ea3121a37
Mike Coughlan <mike.k.coughlan@gmail.com>
2022-12-08 17:22:07 -0500

    updating modeling script documentation

--- modeling.py
+++ modeling.py
@@ -1,6 +1,6 @@
 ##########################################################################################
 #
-#
+#	multi-station-dbdt-risk-assessment/preparing_SW_data.py
 #
 #
 #
@@ -11,22 +11,20 @@
 
 import argparse
 import gc
+import json
 import os
 import pickle
 import random
 
 import numpy as np
 import pandas as pd
-import tensorflow
 import tensorflow as tf
-from sklearn.metrics import mean_squared_error
 from tensorflow.keras.backend import clear_session
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import (BatchNormalization, Conv2D, Dense,
-                                     Dropout, Flatten, MaxPooling2D)
+from tensorflow.keras.layers import (Conv2D, Dense, Dropout, Flatten,
+                                     MaxPooling2D)
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.python.keras.backend import get_session, set_session
+from tensorflow.python.keras.backend import get_session
 
 # stops this program from hogging the GPU
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -37,39 +35,17 @@ except:
     pass
 
 
-CONFIG = {'stations': ['VIC', 'NEW', 'OTT', 'STJ', 'ESK', 'LER', 'WNG', 'NGK', 'BFE'],
-			'thresholds': [7.15],	# list of thresholds to be examined.
-			'params': ['Date_UTC', 'N', 'E', 'sinMLT', 'cosMLT', 'B_Total', 'BY_GSM',
+old_params = ['Date_UTC', 'N', 'E', 'sinMLT', 'cosMLT', 'B_Total', 'BY_GSM',
 	   					'BZ_GSM', 'Vx', 'Vy', 'Vz', 'proton_density', 'T',
-	   					 'AE_INDEX', 'SZA', 'dBHt', 'B'],								# List of parameters that will be used for training.
-	   																							# Date_UTC will be removed, kept here for resons that will be evident below
-			'test_storm_stime': ['2001-03-29 09:59:00', '2001-08-29 21:59:00', '2005-05-13 21:59:00',
-								 '2005-08-30 07:59:00', '2006-12-13 09:59:00', '2010-04-03 21:59:00',
-								 '2011-08-04 06:59:00', '2015-03-15 23:59:00'],						# These are the start times for testing storms
-			'test_storm_etime': ['2001-04-02 12:00:00', '2001-09-02 00:00:00', '2005-05-17 00:00:00',
-									'2005-09-02 12:00:00', '2006-12-17 00:00:00', '2010-04-07 00:00:00',
-									'2011-08-07 09:00:00', '2015-03-19 14:00:00'],	# end times for testing storms. This will remove them from training
-			'plot_stime': ['2011-08-05 16:00', '2006-12-14 12:00', '2001-03-30 21:00'],		# start times for the plotting widow. Focuses on the main sequence of the storm
-			'plot_etime': ['2011-08-06 18:00', '2006-12-15 20:00', '2001-04-01 02:00'],		# end plotting times
-			'plot_titles': ['2011_storm', '2006_storm', '2001_storm'],						# list used for plot titles so I don't have to do it manually
-			'forecast': 30,
-			'window': 30,																	# time window over which the metrics will be calculated
-			'k_fold_splits': 100,													# amount of k fold splits to be performed. Program will create this many models
-			'lead': 12,																# lead time added to each storm minimum in SYM-H
-			'recovery':24,
-			'random_seed':42}															# recovery time added to each storm minimum in SYM-H
+	   					 'AE_INDEX', 'SZA', 'dBHt', 'B']
 
-MODEL_CONFIG = {'version':5,
-					'splits':100,
-					'time_history': 30, 	# How much time history the model will use, defines the 2nd dimension of the model input array
-					'epochs': 100, 		# Maximum amount of empoch the model will run if not killed by early stopping
-					'layers': 1, 		# How many CNN layers the model will have.
-					'filters': 128, 		# Number of filters in the first CNN layer. Will decrease by half for any subsequent layers if "layers">1
-					'dropout': 0.2, 		# Dropout rate for the layers
-					'loss':'mse',
-					'learning_rate': 1e-6,		# Learning rate, used as the inital learning rate if a learning rate decay function is used
-					'lr_decay_steps':230,				# If a learning ray decay funtion is used, dictates the number of decay steps
-					'early_stop_patience':5}
+# loading config and specific model config files. Using them as dictonaries
+with open('config.json', 'r') as con:
+	CONFIG = json.load(con)
+
+with open('model_config.json', 'r') as mcon:
+	MODEL_CONFIG = json.load(mcon)
+
 
 # setting the random seeds for reproducibility
 random.seed(CONFIG['random_seed'])
@@ -116,24 +92,25 @@ def loading_data_and_indicies(station):
 
 
 def create_CNN_model(n_features, loss='categorical_crossentropy', early_stop_patience=3):
-	'''Initializing our model
-		Inputs:
-		model_config: predefined model configuration dictonary
-		n_features: amount of input features into the model
-		loss: loss function to be uesd for training
-		early_stop_patience: amount of epochs the model will continue training once there is no longer val loss improvements'''
+	'''
+	Initializing our model
+
+	Args:
+		n_features (int): number of input features into the model
+		loss (str, optional): loss function to be uesd for training. Defaults to 'categorical_crossentropy'.
+		early_stop_patience (int, optional): number of epochs the model will continue training once there
+												is no longer val loss improvements. Defaults to 3.
+
+	Returns:
+		object: model configuration ready for training
+		object: early stopping conditions
+	'''
 
 
 	model = Sequential()						# initalizing the model
 
 	model.add(Conv2D(MODEL_CONFIG['filters'], 2, padding='same',
 									activation='relu', input_shape=(MODEL_CONFIG['time_history'], n_features, 1)))			# adding the CNN layer
-	# model.add(MaxPooling2D())
-	# model.add(Conv2D(MODEL_CONFIG['filters']*2, (1,2), padding='same', activation='relu'))
-	# model.add(Conv2D(MODEL_CONFIG['filters']*2, 2, padding='same', activation='relu'))
-	# model.add(MaxPooling2D())
-	# model.add(Conv2D(MODEL_CONFIG['filters']*4, (1,2), padding='same', activation='relu'))
-	# model.add(Conv2D(MODEL_CONFIG['filters']*4, (1,2), padding='same', activation='relu'))
 	model.add(MaxPooling2D())
 	model.add(Flatten())							# changes dimensions of model. Not sure exactly how this works yet but improves results
 	model.add(Dense(MODEL_CONFIG['filters'], activation='relu'))		# Adding dense layers with dropout in between
@@ -177,30 +154,39 @@ def fit_CNN(model, xtrain, xval, ytrain, yval, early_stop, split, station, first
 		model.fit(Xtrain, ytrain, validation_data=(Xval, yval),
 					verbose=1, shuffle=True, epochs=MODEL_CONFIG['epochs'], callbacks=[early_stop])			# doing the training! Yay!
 
+		# checking that the model directory exists. Creates it if not.
 		if not os.path.exists('models/{0}'.format(station)):
 			os.makedirs('models/{0}'.format(station))
 
-		model.save('models/{0}/CNN_SW_only_split_{2}.h5'.format(station, split))
+		# saving the model
+		model.save('models/{0}/CNN_SW_only_split_{1}.h5'.format(station, split))
 
 	if not first_time:
 
-		model = load_model('models/{0}/CNN_SW_only_split_{2}.h5'.format(station, split))				# loading the models if already trained
+		# loading the model if it has already been trained.
+		model = load_model('models/{0}/CNN_SW_only_split_{1}.h5'.format(station, split))				# loading the models if already trained
 
 	return model
 
 
 def making_predictions(model, test_dict, split):
-	'''function used to make the predictions with the testing data
-		Inputs:
-		model: pre-trained model
-		test_dict: dictonary with the testing model inputs and the real data for comparison
-		thresholds: which threshold is being examined
-		split: which split is being tested'''
+	'''
+	Function using the trained models to make predictions with the testing data.
 
-	for key in test_dict:									# looping through the sub dictonaries for each storm
+	Args:
+		model (object): pre-trained model
+		test_dict (dict): dictonary with the testing model inputs and the real data for comparison
+		split (int): which split is being tested
+
+	Returns:
+		dict: test dict now containing columns in the dataframe with the model predictions for this split
+	'''
+
+	# looping through the sub dictonaries for each storm
+	for key in test_dict:
 
 		Xtest = test_dict[key]['Y']							# defining the testing inputs
-		Xtest = Xtest.reshape((Xtest.shape[0], Xtest.shape[1], Xtest.shape[2], 1))				# reshpaing for one channel input
+		Xtest = Xtest.reshape((Xtest.shape[0], Xtest.shape[1], Xtest.shape[2], 1))			# reshpaing for one channel input
 		print('Test input Nans: '+str(np.isnan(Xtest).sum()))
 
 		predicted = model.predict(Xtest, verbose=1)						# predicting on the testing input data
@@ -211,55 +197,58 @@ def making_predictions(model, test_dict, split):
 
 		df = test_dict[key]['real_df']									# calling the correct dataframe
 		df['predicted_split_{0}'.format(split)] = predicted		# and storing the results
-		# df.dropna(inplace=True)
 		re = df['crossing']
 
+		# checking for nan data in the results
 		print('Pred has Nan: '+str(predicted.isnull().sum()))
 		print('Real has Nan: '+str(re.isnull().sum()))
-		# print('RMSE: '+str(np.sqrt(mean_squared_error(re,df['predicted_split_{0}'.format(split)]))))
 
 	return test_dict
 
 
 def main(station):
+	'''
+	Pulls all the above functions together. Loops through the number of splits to create, fit ,
+	and predict with a unique model for each train-val split. Outputs a saved file with the results.
 
+	Args:
+		station (str): 3 diget code for the station being examined. Passed to the script via arg parsing.
+	'''
+
+	# loading all data and indicies
 	train_dict, test_dict, train_indicies, val_indicies = loading_data_and_indicies(station)
 
-	for split in range(MODEL_CONFIG['splits']):		# this is the bulk of the K fold. We loop through the list of indexes and train on the different train-val indices
+	# this is the bulk of the shuffeled k-fold splitting. We loop through the list of indexes and train on the different train-val indices
+	for split in range(MODEL_CONFIG['splits']):
 
 		train_index = train_indicies['split_{0}'.format(split)].to_numpy()
 		val_index = val_indicies['split_{0}'.format(split)].to_numpy()
 
 		print('Split: '+ str(split))
+
+		# clearing the information from any old models so we can run clean new ones.
 		if 'MODEL' in locals():
-			reset_keras(MODEL)			# clearing the information from any old models so we can run clean new ones.
+			reset_keras(MODEL)
 		MODEL, early_stop = create_CNN_model(n_features=train_dict['X'].shape[2], loss='categorical_crossentropy', early_stop_patience=5)					# creating the model
-		# print(MODEL.summary())
 
 		# pulling the data and catagorizing it into the train-val pairs
 		xtrain = train_dict['X'][train_index]
-		array_sum = np.sum(xtrain)
-		print(np.isnan(array_sum))
 		xval =  train_dict['X'][val_index]
-		array_sum = np.sum(xval)
-		print(np.isnan(array_sum))
 		ytrain = train_dict['crossing'][train_index]
-		array_sum = np.sum(ytrain)
-		print(np.isnan(array_sum))
 		yval = train_dict['crossing'][val_index]
-		array_sum = np.sum(yval)
-		print(np.isnan(array_sum))
 
-		if os.path.exists('models/{0}/CNN_SW_only_split_{2}.h5'.format(station, MODEL_CONFIG['version'], split)):
-			model = fit_CNN(MODEL, xtrain, xval, ytrain, yval, early_stop, split, station, first_time=False)			# does the model fit!
+		# if the saved model already exists, loads the pre-fit model
+		if os.path.exists('models/{0}/CNN_SW_only_split_{1}.h5'.format(station, split)):
+			model = fit_CNN(MODEL, xtrain, xval, ytrain, yval, early_stop, split, station, first_time=False)
 
+		# if model has not been fit, fits the model
 		else:
-			model = fit_CNN(MODEL, xtrain, xval, ytrain, yval, early_stop, split, station, first_time=True)			# does the model fit!
+			model = fit_CNN(MODEL, xtrain, xval, ytrain, yval, early_stop, split, station, first_time=True)
 
 		test_dict = making_predictions(model, test_dict, split)					# defines the test dictonary for storing results
 
 
-	# for each storm we set the datetime index and saves the data in a CSV file
+	# for each storm we set the datetime index and saves the data in a CSV/feather file
 	for i in range(len(test_dict)):
 		real_df = test_dict['storm_{0}'.format(i)]['real_df']
 		pd.to_datetime(real_df['Date_UTC'], format='%Y-%m-%d %H:%M:%S')
